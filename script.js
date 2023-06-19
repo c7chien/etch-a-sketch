e5bff2ff6ba3a8ad11a8b701ca09f1555aaa3f72
Calvin Chien <calvin2chien@gmail.com>
2023-06-19 00:09:56 -0400

    Add button to reset grid size.

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 
-for (let rows = 0; rows <8; rows++){
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add("grid","row",`row${rows}`);
-    //rowDiv.textContent = "grid.row:" + rows;
-    for (let columns = 0; columns<8; columns++){
-        const colDiv = document.createElement("div");
-        colDiv.classList.add("grid","column",`col${columns}`);
-        colDiv.textContent = `grid.${rows}.${columns}`;
-        rowDiv.appendChild(colDiv);
-    }
-document.body.appendChild(rowDiv);
+// Build 16x16 grid
+function createGrid(size){
+    const fullGrid = document.createElement("div");
+    fullGrid.setAttribute("id","fullGrid");
+    for (let rows = 0; rows <size; rows++){
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("grid","row",`row${rows}`);
+        //rowDiv.textContent = "grid.row:" + rows;
+        for (let columns = 0; columns<size; columns++){
+            const colDiv = document.createElement("div");
+            colDiv.classList.add("grid","pixel","column",`coor-${rows}-${columns}`);
+            //colDiv.textContent = `${rows}.${columns}`;
+            rowDiv.appendChild(colDiv);
+        }
+    fullGrid.appendChild(rowDiv);
+    }    
+    document.body.appendChild(fullGrid);
 }
 
+function deleteGrid(){
+    const fullGrid = document.querySelector("#fullGrid");
+    console.log("delete grid");
+    console.log(fullGrid);
+    fullGrid.remove();
 
-const allDiv = Array.from(document.querySelectorAll("div"));
+}
 
-console.dir(allDiv);
+function changeColor(){
+    // console.dir(this);
+    this.classList.toggle("hover");
+}
+
+const allDiv = Array.from(document.querySelectorAll(".pixel"));
+
+// Initiate grid
+createGrid(16);
+
+allDiv.forEach(grid => grid.addEventListener("mouseover",changeColor));
+
+function changeGridSize(){
+    // cap gridSize to 100 for performance
+    gridSize = Math.min(100,parseInt(prompt("Set a grid size: ","10")));
+    deleteGrid();
+    createGrid(gridSize);
+}
+
+const gridButton = document.querySelector(".gridbutton");
+gridButton.addEventListener("click",changeGridSize);
+
+
 
